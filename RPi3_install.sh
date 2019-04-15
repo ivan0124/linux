a97f2c72d603e19ea946d12c8ec9101bb24a0aea
ivan1_chen <ivan1_chen@askey.com>
2019-04-15 18:09:28 +0800

    modify script for TPM2.0

--- RPi3_install.sh
+++ RPi3_install.sh
@@ -4,9 +4,14 @@ tar xvzf ./sd_card.tgz
 sudo rm -rf /lib/firmware
 sudo rm -rf /lib/modules
 sudo rm -rf /boot/overlays
+echo "remove /lib/firmware /lib/modules /boot/overlays"
 sudo cp -rf ./sd_card/lib/firmware/ /lib
+echo "cp -rf ./sd_card/lib/firmware/ /lib done"
 sudo cp -rf ./sd_card/lib/modules/ /lib
+echo "cp -rf ./sd_card/lib/modules/ lib done"
 sudo cp -rf ./sd_card/boot/* /boot
+echo "cp -rf ./sd_card/boot/* /boot done"
 sync
 sync
 sync
+echo "sync and install done"
--- config.sh
+++ config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 patch -p1 < RPi_3_SPI_TPM_Driver_And_Automatic_Driver_Load.patch
+#config.gz is from RPi3.
+#In RPi3, please type instruction "$sudo modprobe configs" and get file from "/proc/config.gz"
+#$scp pi@10.1.112.15:/proc/config.gz ./
 zcat ./config.gz > .config
